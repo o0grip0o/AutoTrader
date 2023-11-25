8f561eac3a14798071b0ec6358905a57746ec451
Ossie Bodero <ossie.boder@outlook.com>
2023-11-25 17:10:42 -0500

    creating fucntions and main

---
+++ autotrader.py
@@ -0,0 +1,25 @@
+import pandas as pds
+
+def get_data():
+    ...
+
+def get_model():
+    ...
+
+def get_predictions():
+    ...
+
+def get_recommendations():
+    ...
+
+def get_portfolio():
+    ...
+
+def get_trades():
+    ...
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
